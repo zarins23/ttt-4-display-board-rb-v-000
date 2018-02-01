33c696960ca77ca13907a4190b1f060f6d394e91
Zarin Shahab <zarins23@gmail.com>
2018-02-01 07:21:41 +0000

    Done.

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 def display_board(array)
   puts " #{array[0]} | #{array[1]} | #{array[2]} "
-  puts "-----------\n"
+  puts "-----------"
   puts " #{array[3]} | #{array[4]} | #{array[5]} "
-  puts "-----------\n"
+  puts "-----------"
   puts " #{array[6]} | #{array[7]} | #{array[8]} "
 end
